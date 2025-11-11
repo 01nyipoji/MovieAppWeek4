5eabbbe9e813d81e0144e0774c50e7857ef96db8
Nyipoji Kani <nyip2019@gmail.com>
2025-11-11 22:25:45 +0100

    intial commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const SearchBar = ({onSearch}) => {
+    const [term, setTerm] = useState('');
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        onSearch(term);
+    }
+  return <form onSubmit={handleSubmit}>
+    <input type="text" value={term} onChange={(e)=> setTerm(e.target.value)} placeholder='Search movies...' className='input input-success' />
+  </form>
+}
+
+export default SearchBar
